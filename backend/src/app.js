d016bafec6941bf9ee2440fd8550c42e6f0d0888
Bhavish-rai <raibhavish007@gmail.com>
2026-09-19 14:45:21 +0530

    feat(dev-b): add campaign management

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const { success } = require("./utils/response");
 
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
+const campaignRoutes = require("./routes/campaign.routes");
 
 const app = express();
 
@@ -36,6 +37,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 // Campaigns
+app.use("/api/campaigns", campaignRoutes);
+
 // Donations
 // Allocations
 // Expenses
